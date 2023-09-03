06cd8f7aa8046db351a1fb606d94e3042a5d7ea5
guiRosse <rosseguillaume0890@gmail.com>
2023-09-03 21:52:48 +0400

    stages of the hangman + losing and winn-cons

--- Day-7-Hangman-1-Start/hangman.py
+++ Day-7-Hangman-1-Start/hangman.py
@@ -2,9 +2,69 @@
 import random
 import cls #    Import a small fn for clearing screen conveniently
 
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+stage_num = len(stages)-1
 word_list = ["aardvark", "baboon", "camel"]
 blanks = []         #   A placeholder for my blanks for the next steps
 mystery_word = ""   #   Only used to displaay the list items of [blanks] as an actual word
+has_won = False # Variable for letting the game go on with winning chances
+has_lost = False # Variable for letting the game go on as losing
 
 cls.clear_screen()
 
@@ -25,23 +85,31 @@ print(f"Your word is {mystery_word}")
 # the letters in the chosen_word and 'display' has no more 
 # blanks ("_"). Then you can tell the user they've won.
 
-has_won = False # Variable for letting the game go on
-
 print("\n\n*******************************************************************")
 
-while not has_won:
+while not has_won and not has_lost:
     #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
     guess = input("\n\nChoose a letter: ").lower()
     #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
     #TODO-3.1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-    for index, letter in enumerate(chosen_word):
-        if guess == letter:
-            # print(index)
-            # print(letter)
-            blanks[index] = letter   # Blanks list item get to be replaced by the guessed letter
-            
-    print(blanks)
+    if guess in chosen_word:
+        for index, letter in enumerate(chosen_word):
+            if guess == letter:
+                # print(index)
+                # print(letter)
+                blanks[index] = letter   # Blanks list item get to be replaced by the guessed letter
+        
+
+    elif stage_num == 0:
+        has_lost = True
+        print(stages[stage_num])
+        print("Game over! You lose")
+    else:
+        print(stages[stage_num])
+        stage_num-=1
+    
+    print(f"{' '.join(blanks)}")
 
     if "_" not in blanks:
         has_won = True
