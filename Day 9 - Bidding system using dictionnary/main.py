0dc679f1490143652840d2b7aad4053e821181ff
guiRosse <rosseguillaume0890@gmail.com>
2023-09-05 22:52:13 +0400

    finding highest bid in Fn

--- Day 9 - Bidding system using dictionnary/main.py
+++ Day 9 - Bidding system using dictionnary/main.py
@@ -4,13 +4,20 @@ from logo import logo
 is_bidding = "y"
 bidder = ""
 bid = 0
-max_bid = 0
-max_bidder = ""
-
 bid_dict = {}
 
 def cls():
     os.system('cls')
+
+def find_highest_bid(bids):
+    max_bid = 0
+    max_bidder = ""
+    for bidder in bids:
+        if bids[bidder] > max_bid:
+            max_bid = bids[bidder]
+    
+    max_bidder = bidder
+    return max_bidder
 
 cls()
 
@@ -27,10 +34,8 @@ while is_bidding.lower() in ["yes","y"]:
         cls()
     else:
         cls()
-        for bidder in bid_dict:
-            if bid_dict[bidder] > max_bid:
-                max_bid = bid_dict[bidder]
-                max_bidder = bidder
+        max_bidder = find_highest_bid(bid_dict)
+        max_bid = bid_dict[max_bidder]
 
 cls()
 print(f"Congrats to {max_bidder.capitalize()}. You won the bid with ${max_bid}!!")
